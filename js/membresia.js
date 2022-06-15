4721b690779a7ecd68367da27129e70c7ce49380
aguspeiretti <aguspeiretti@gmail.com>
2022-06-15 11:13:58 -0300

    tercer commit

---
+++ js/membresia.js
@@ -0,0 +1,104 @@
+//CREAR CARDS
+
+class Membresias {
+  constructor(imagen, titulo, precio, descripcion) {
+    this.imagen = imagen;
+    this.titulo = titulo;
+    this.precio = precio;
+    this.descripcion = descripcion;
+  }
+}
+const mensual = new Membresias(
+  "../img/mensual.svg",
+  "mensual",
+  "$10000",
+  "membresia valida por 30 dias."
+);
+
+const Anual = new Membresias(
+  "../img/anual.svg",
+  "Anual",
+  "$70000",
+  "membresia valida por 365 dias."
+);
+
+const dePorVida = new Membresias(
+  "../img/ps.svg",
+  "De por vida ",
+  "$150000",
+  "membresia valida por siempre."
+);
+
+let carrito = [];
+
+const conjuntoMembresias = [mensual, Anual, dePorVida];
+
+const cardContainer = document.getElementById("cardContainer");
+
+conjuntoMembresias.forEach((membresia) => {
+  const card = document.createElement("div");
+  card.innerHTML = `
+  <div class="box" id="box">
+  <div class="imgBx">
+  <img src=${membresia.imagen}  alt="credencial" />
+  </div>
+  <div class="content">
+  <h2>${membresia.titulo}</h2>
+    <p>${membresia.descripcion}</p>
+    <p>${membresia.precio}</p>
+    <button  data-id="${membresia.titulo}"class="btnCard">Comprar</button>
+
+</div>
+`;
+  cardContainer.append(card);
+});
+const carr = document.querySelector("#carr");
+
+const mostrarCarrito = () => {
+  carr.innerHTML = "";
+  carrito.forEach((membresia) => {
+    const carring = document.createElement("div");
+    carring.className = "carring";
+    carring.innerHTML = `
+        <img src=${membresia.imagen} alt="" />
+        <h2>${membresia.titulo}</h2>
+        <p>${membresia.descripcion}</p>
+        <p>${membresia.precio}</p>
+        <button id="vaciarCarrito"> Eliminar </button>
+        `;
+    carr.append(carring);
+  });
+};
+
+const agregarAlCarrito = (e) => {
+  const eleccion = e.target.getAttribute("data-id");
+  console.log(eleccion);
+
+  const memb = conjuntoMembresias.find(
+    (membresia) => membresia.titulo == eleccion
+  );
+  carrito.push(memb);
+  mostrarCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+const btnCompra = document.querySelectorAll(".btnCard");
+
+btnCompra.forEach((btn) => {
+  btn.addEventListener("click", agregarAlCarrito);
+});
+
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"));
+  mostrarCarrito();
+}
+
+const vaciarCarrito = () => {
+  if (localStorage.getItem("carrito")) {
+    localStorage.removeItem("carrito");
+  }
+  carrito = [];
+  mostrarCarrito();
+};
+
+const eliminarCarrito = document.querySelector("#vaciarCarrito");
+eliminarCarrito.addEventListener("click", vaciarCarrito);
---
+++ js/usuario.js
@@ -0,0 +1,37 @@
+// CREAR USUARIO
+
+class Usuarios {
+  constructor(nombre, apellido, clave, clave2) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.clave = clave;
+    this.clave2 = clave2;
+  }
+}
+
+const crearUsuario = () => {
+  const baseUsuarios = [];
+
+  const usuario = new Usuarios(
+    document.getElementById("nombre").value,
+    document.getElementById("apellido").value,
+    document.getElementById("clave").value,
+    document.getElementById("clave2").value
+  );
+
+  if (
+    usuario.clave.length >= 8 &&
+    usuario.clave2.length >= 8 &&
+    usuario.clave === usuario.clave2
+  ) {
+    alert("usuario registrado");
+    baseUsuarios.push(usuario);
+  } else {
+    alert("error en el registro de usuario o password intenta nuevamente");
+  }
+
+  console.log(baseUsuarios);
+};
+
+const botonRegistro = document.querySelector("#btn");
+botonRegistro.addEventListener("click", crearUsuario);
